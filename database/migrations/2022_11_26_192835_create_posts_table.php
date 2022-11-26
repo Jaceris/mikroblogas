6138a519148563d0a1ad069f9fed56fa8eb6f208
Mindaugas Jaceris <mindaugas.jaceris@gmail.com>
2022-11-27 00:14:03 +0200

    Factories & seeders & controllers

--- database/migrations/2022_11_26_192835_create_posts_table.php
+++ database/migrations/2022_11_26_192835_create_posts_table.php
@@ -17,7 +17,7 @@ return new class extends Migration
             $table->id();
             $table->string('title');
             $table->string('content');
-            $table->boolean('private');
+            $table->boolean('private')->default(0);
             $table->foreignId('user_id')
                 ->constrained()
                 ->onDelete('cascade');
